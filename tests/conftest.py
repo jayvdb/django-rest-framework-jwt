24adb43a889a8fe2adb56691092bef88ab58def4
John Vandenberg <jayvdb@gmail.com>
2019-04-20 18:31:17 +0700

    Add messages app

--- tests/conftest.py
+++ tests/conftest.py
@@ -36,6 +36,7 @@ def pytest_configure():
             'django.contrib.auth',
             'django.contrib.contenttypes',
             'django.contrib.sessions',
+            'django.contrib.messages',
             'django.contrib.sites',
             'django.contrib.staticfiles',
 
